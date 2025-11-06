fa835c4416260d05c38349f8ca6c6ecf1e8bc0df
ivasx <ambroziak.v.ivan@gmail.com>
2025-11-06 22:20:41 +0200

    Add animations and sprites

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,15 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
-    public static GameInput Instance { get; private set; }
-    
+    public static GameInput Instance  { get; private set; }
     private PlayerInputActions playerInputActions;
-    
+
     private void Awake()
     {
-        Instance = this;
-        
+        Instance  = this;
         playerInputActions = new PlayerInputActions();
         playerInputActions.Enable();
     }
@@ -18,5 +17,11 @@ public class GameInput : MonoBehaviour
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         return inputVector;
+    }
+
+    public Vector3 GetMousePosition()
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        return mousePos;
     }
 }
