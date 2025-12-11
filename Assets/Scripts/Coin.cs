071b4143724201be0f7b972cb0f3bcbb475fd3b2
ivasx <ambroziak.v.ivan@gmail.com>
2025-12-11 20:07:46 +0200

    Add house prefab

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,13 +4,13 @@ public class Coin : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Перевіряємо, чи це гравець
+
         if (other.GetComponent<Player>() != null)
         {
-            // Кажемо менеджеру зарахувати монету
+
             GameManager.Instance.CoinCollected();
             
-            // Знищуємо саму монетку
+
             Destroy(gameObject);
         }
     }
